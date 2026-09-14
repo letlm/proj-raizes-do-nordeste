730ebddbfd22ca90d80e7b0f252aa7eddb100340
letlm <lealmleticia@gmail.com>
2026-09-14 19:18:49 -0300

    chore: ajustes nas configurações de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { HealthCheckModule } from './healthcheck/healthcheck.module';
+import { UserModule } from './api/modules/user.module';
+import { UnitModule } from './api/modules/unit.module';
+import { ProductModule } from './api/modules/product.module';
+import { UnitStockModule } from './api/modules/unitStock.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HealthCheckModule,
+    UserModule,
+    UnitModule,
+    ProductModule,
+    UnitStockModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
